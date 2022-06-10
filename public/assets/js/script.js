f6facd8e87d7c2a6919361ad09c88532084e0ce9
Josiah-Canada <josiahcanada@yahoo.com>
2022-06-09 21:53:58 -0400

    added functionality

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,24 @@
 const $animalForm = document.querySelector('#animal-form');
 
 const handleAnimalFormSubmit = event => {
+  fetch('/api/animals', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(animalObject)
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      alert('Error: ' + response.statusText);
+    })
+    .then(postResponse => {
+      console.log(postResponse);
+      alert('Thank you for adding an animal!');
+    });
   event.preventDefault();
 
   // get animal data and organize it
